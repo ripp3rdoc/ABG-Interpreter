4f67bba2e94aa205e8a5a5b965e03a749c038554
talson <talsonservice@gmail.com>
2022-06-20 06:00:57 +0300

    fixed form bug

--- js/main.js
+++ js/main.js
@@ -1,67 +1,81 @@
 function getValues() {
-  ph = document.getElementById("ph").value;
-  co2 = document.getElementById("co2").value;
-  hco3 = document.getElementById("hco3").value;
-  pao2 = document.getElementById("pao2").value;
+  // let ph = document.getElementById("ph").value;
+  // let co2 = document.getElementById("co2").value;
+  // let hco3 = document.getElementById("hco3").value;
+  //   let pao2Value = document.getElementById("pao2").value;
 }
-
-function calculateOxygen(pao2) {
-  if (pao2 >= 80 && pao2 <= 100) {
+var form = document.getElementById("form");
+function handleForm(event) {
+  event.preventDefault();
+}
+form.addEventListener("submit", handleForm);
+function oxy(pao2Value) {
+  console.log("Oxy func: " + pao2Value);
+  if (pao2Value >= 80 && pao2Value <= 100) {
     return "With normal oxygenation.";
-  } else if (pao2 >= 60 && pao2 <= 79) {
+  } else if (pao2Value >= 60 && pao2Value <= 79) {
     return "With mild hypoxemia.";
-  } else if (pao2 >= 40 && pao2 <= 59) {
+  } else if (pao2Value >= 40 && pao2Value <= 59) {
     return "With moderate hypoxemia.";
   } else {
     return "With severe hypoxemia.";
   }
 }
-
-if (pao2 == 0 || pao2 == null || pao2 == undefined) {
-  return pao2 == null;
-} else {
-  oxygenResult = calculateOxygen(pao2);
-}
-
-function calculateValues() {
-  if (
-    ph >= 7.35 &&
-    ph <= 7.45 &&
-    co2 >= 35 &&
-    co2 <= 45 &&
-    hco3 >= 22 &&
-    hco3 <= 26
-  ) {
-    if (pao2 == !null) {
-      return "The patient is normal " + oxygenResult;
-    } else {
-      return "The patient is Normal.";
-    }
-  } else if ((ph <= 7.35 && co2 > 45) || (ph < 7.35 && co2 >= 45)) {
-    if (pao2 == !null) {
-      return "The patient is normal " + oxygenResult;
-    } else {
-      return "Respiratory acidosis";
-    }
-  } else if ((ph >= 7.35 && co2 < 45) || (ph > 7.35 && co2 <= 45)) {
-    if (pao2 == !null) {
-      return "The patient is normal " + oxygenResult;
-    } else {
-      return "respiratory alkalosis";
-    }
-  } else if ((ph <= 7.35 && hco3 < 26) || (ph < 7.35 && hco3 <= 26)) {
-    if (pao2 == !null) {
-      return "The patient is normal " + oxygenResult;
-    } else {
-      return "Metabolic acidosis";
-    }
-  } else if ((ph >= 7.35 && hco3 > 26) || (ph > 7.35 && hco3 >= 26)) {
-    if (pao2 == !null) {
-      return "The patient is normal " + oxygenResult;
-    } else {
-      return "Metabolic alkalosis";
-    }
+function calculateOxygen() {
+  let pao2Value = document.getElementById("pao2").value;
+  if (pao2Value == 0 || pao2Value == null || pao2Value == undefined) {
+    console.log("if pao2Value null: " + pao2Value);
+    return pao2Value == null;
   } else {
-    return "Something went wrong";
+    console.log("if pao2Value !null: " + pao2Value);
+    oxygenResult = oxy(pao2Value);
+
+    document.getElementById("resultsAnalysis").textContent =
+      "1: PaO2 value is: " + oxygenResult;
+    document.getElementById("resultsOxygen").textContent =
+      "2: PaO2 value is: " + oxygenResult;
   }
 }
+
+// function calculateValues() {
+//   if (
+//     ph >= 7.35 &&
+//     ph <= 7.45 &&
+//     co2 >= 35 &&
+//     co2 <= 45 &&
+//     hco3 >= 22 &&
+//     hco3 <= 26
+//   ) {
+//     if (pao2 == !null) {
+//       return "The patient is normal " + oxygenResult;
+//     } else {
+//       return "The patient is Normal.";
+//     }
+//   } else if ((ph <= 7.35 && co2 > 45) || (ph < 7.35 && co2 >= 45)) {
+//     if (pao2 == !null) {
+//       return "The patient is normal " + oxygenResult;
+//     } else {
+//       return "Respiratory acidosis";
+//     }
+//   } else if ((ph >= 7.35 && co2 < 45) || (ph > 7.35 && co2 <= 45)) {
+//     if (pao2 == !null) {
+//       return "The patient is normal " + oxygenResult;
+//     } else {
+//       return "respiratory alkalosis";
+//     }
+//   } else if ((ph <= 7.35 && hco3 < 26) || (ph < 7.35 && hco3 <= 26)) {
+//     if (pao2 == !null) {
+//       return "The patient is normal " + oxygenResult;
+//     } else {
+//       return "Metabolic acidosis";
+//     }
+//   } else if ((ph >= 7.35 && hco3 > 26) || (ph > 7.35 && hco3 >= 26)) {
+//     if (pao2 == !null) {
+//       return "The patient is normal " + oxygenResult;
+//     } else {
+//       return "Metabolic alkalosis";
+//     }
+//   } else {
+//     return "Something went wrong" + oxygenResult;
+//   }
+// }
